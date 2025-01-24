ebad139aa0880ce5173f71e27f52de1bac05eede
Richard Cossettini <rcossett@c2r9s3.42lausanne.ch>
2025-01-24 15:10:24 +0100

    with libft

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -1,39 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rcossett <rcossett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/09 15:54:35 by rcossett          #+#    #+#             */
-/*   Updated: 2025/01/24 14:59:44 by rcossett         ###   ########.fr       */
+/*   Created: 2025/01/24 14:54:33 by rcossett          #+#    #+#             */
+/*   Updated: 2025/01/24 14:54:56 by rcossett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
-
-# include <stdlib.h>
-# include <unistd.h>
-# include <stdio.h>
-
-#endif
-
-typedef struct		s_node
-{
-	int				value;
-	int				position;
-	struct s_node	*next;
-}					t_node;
-
-typedef struct		s_stack
-{
-	t_node			*top;
-	t_node			*bottom;
-	int				size;
-}					t_stack;
-
-// PROTOTYPES
-
-t_node				*find_smallest_value(t_stack *stack);
 
